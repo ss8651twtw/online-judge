74ed9b7d8e35a3b8aedf815d6423829737b7b1cd
ss8651twtw <ss8651twtw@gmail.com>
2016-03-10 01:14:37 +0800

    10016

---
+++ nctuoj/10016.cpp
@@ -0,0 +1,23 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <utility>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <algorithm>
+#define PB push_back
+using namespace std;
+int main(){
+	int a,b;
+	scanf("%d%d",&a,&b);
+	if(__gcd(a,b)!=1)puts("-1");
+	for(int i=0;i<b;i++)
+		if((i*a)%b==1){
+			printf("%d\n",i);
+			break;
+		}
+}
